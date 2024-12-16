76335d2e94d0b3fded2cb1c142ae71b2ae186147
Quang Huy <kaheetonaa@gmail.com>
2024-12-16 11:04:51 +0100

    update

--- main.py
+++ main.py
@@ -1,49 +1,22 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-st.write("This is :blue[test]")
-
-data_df = pd.DataFrame(
-    {
-        "name":['a','b','c','d'],
-        "sales": [
-            ["This is :blue[test]", '4', '26', '80', 'a', '40'],
-            ["This is :blue[test]", '4', '26', '80', 'a', '40'],
-            ["This is :blue[test]", '4', '26', '80', 'a', '40'],
-            ["This is :blue[test]", '4', '26', '80', 'a', '40'],
-        ],
-    }
-)
-
-st.data_editor(
-    data_df,
-    column_config={
-        "name": st.column_config.TextColumn(
-        "Name", help="The name of the user", max_chars=100
-    ),
-        "sales": st.column_config.ListColumn(
-            "Sales (last 6 months)",
-            help="The sales volume in the last 6 months",
-            width="large",
-        ),
-    },
-    hide_index=True,
-)
-
+columns=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q']
 np.random.seed(0)
-df2 = pd.DataFrame(np.random.randn(10,4), columns=['A','B','C','D'])
-df2.style
+df2 = pd.DataFrame(np.zeros(shape=(15,17)).astype('U'), columns=columns)
 
-def style_negative(v, props=''):
-    return props if v < 0 else None
-s2 = df2.style.map(style_negative, props='color:red;')\
-              .map(lambda v: 'opacity: 20%;' if (v < 0.3) and (v > -0.3) else None)
+for i in range(17):
+    df2.loc[1,columns[i]]="<span style='display: inline-block; border-radius: 3px; padding: .2em .5em .3em; border-radius: 2px; background: darkgray; color: white; font-weight: 600; margin: .25em .1em'>1.1.1</span>"
+    if i<9:
+        df2.loc[0,columns[i]]="<img src='https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0"+str(i+1)+".jpg'></img>"
+    else:
+        df2.loc[0,columns[i]]="<img src='https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-"+str(i+1)+".jpg'></img>"
 
-def highlight_max(s, props=''):
-    return np.where(s == np.nanmax(s.values), props, '')
-s2.apply(highlight_max, props='color:white;background-color:darkblue', axis=0)  
-
-s2
+df2.loc[2,'B']="<span style='display: inline-block; border-radius: 3px; padding: .2em .5em .3em; border-radius: 2px; background: darkgreen; color: white; font-weight: 600; margin: .25em .1em'>1.1.1</span>"
+st.title('SDG and Humanitarian Mapping Monitoring')
+st.write('---')
+st.markdown(df2.style.hide(axis = 0).hide(axis = 1).to_html(), unsafe_allow_html = True)
