730c700af5564b8f626be5e8c495432da614befd
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-04-28 11:36:39 +0200

    update

--- pages/submitter.py
+++ pages/submitter.py
@@ -26,7 +26,7 @@ code=st.text_input(label='input project code here')
 if code:
     match platform:
         case 'HOT-TM':
-            data=requests.get('https://tasking-manager-production-api.hotosm.org/api/v2/projects/'+code+'/',headers={"accept":"application/json","Authorization":"TOK:<TVRBek5ERTVPRGsuYUFkTlZnLkFZU3NCNnpyV3ZFeG5QdnIzOWo1WTJnYlRqdw==>"})#.json()
+            data=requests.get('https://tasking-manager-production-api.hotosm.org/api/v2/projects/'+code+'/',headers={"accept":"application/json","Authorization":"TVRBek5ERTVPRGsuYUFkTlZnLkFZU3NCNnpyV3ZFeG5QdnIzOWo1WTJnYlRqdw=="})#.json()
             st.write(data)
             st.write('Link: https://tasks.hotosm.org/projects/'+code)
             st.write('Name:'+data['projectInfo']['name'])
